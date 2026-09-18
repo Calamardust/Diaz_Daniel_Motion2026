55c1b2c6d708cbd7659a79061392624203de0a22
diazperd <diazperd@OA-S213-09>
2026-09-18 16:25:26 -0400

    Task 2 and 3 completed, the spawn of  bombs on the corners of the player works and happens randomly when the h key is pressed, and the player ships moves halfway to the enemy ship every time the left mouse is clicked.

--- Daniel_Diaz_Motion2026/Assets/Scripts/Controllers/Player.cs
+++ Daniel_Diaz_Motion2026/Assets/Scripts/Controllers/Player.cs
@@ -12,11 +12,6 @@ public class Player : MonoBehaviour
 
     public Transform playerPost;// player position
 
-
-    Vector2 spawnOffset = Vector2.down;
-
-
-
     void Update()
     {
         if (Keyboard.current.spaceKey.wasPressedThisFrame)// normalizes a vector and prints it to the console (done in class)
@@ -26,11 +21,19 @@ public class Player : MonoBehaviour
 
         if (Keyboard.current.bKey.wasPressedThisFrame)// spawns a bomb relative to the player position when b is pressed
         {
-            SpawnBombAtOffset(transform.up);
+            SpawnBombAtOffset(transform.up);// calls a bomb in front of the player 
         }
         if (Keyboard.current.tKey.wasPressedThisFrame)// spawns a trail of bombs below the player position when t is pressed
         {
             SpawnBombTrail(0.8f, 5);// calls the trail spawn fuction with a spacing of 0.5 and 5 bombs totalS
+        }
+        if (Keyboard.current.hKey.wasPressedThisFrame)// spawns a bomb at a random corner when h is pressed
+        {
+            SpawnBombOnRandomCorner(1f);// calls the random corner spawn function with a certain distance 
+        }
+        if (Mouse.current.leftButton.wasPressedThisFrame)// warps the player when the left mouse button is pressed
+        {
+           WarpPlayer(enemyTransform, 0.5f);// calls the warp function and move the player towards the nearest enemy with a ratio betwen 0 and 1 (0.5)
         }
     }
 
@@ -39,7 +42,7 @@ public class Player : MonoBehaviour
         Instantiate(bombPrefab, playerPost.position + (Vector3)inOffset, Quaternion.identity);// instantiates a bomb relative to the player position with an offset
     }
 
-    public  void SpawnBombTrail(float inBombSpacing, int inNumberOfBombs) // spawns a trail of bombs with a specified spacing and number of bombs
+    public void SpawnBombTrail(float inBombSpacing, int inNumberOfBombs) // spawns a trail of bombs with a specified spacing and number of bombs
     {
         for (int i = 1; i <= inNumberOfBombs; i++) // for loop for spawning bombs 
         {
@@ -48,16 +51,29 @@ public class Player : MonoBehaviour
 
     }
 
+    public void SpawnBombOnRandomCorner(float inDistance)// spawns a bomb at a random corner 
+    {
+        List<Vector2> corners = new List<Vector2> 
+        { new Vector2(-1f, 1f), new Vector2(1f, 1f), new Vector2(1f, -1f), new Vector2(-1f, -1f) }; // creates a list of the four corners>
+
+        int randomIndex = Random.Range(0, corners.Count); // generates a random index to select a corner
+        Vector2 randomCorner = corners[randomIndex] * inDistance; // selects a random corner and scales it by the distance
+        Instantiate(bombPrefab, playerPost.position + (Vector3)randomCorner, Quaternion.identity); // spawns the bomb at the random corner 
+    }
+
+
     public static Vector2 Normalizer(Vector2 normalized)// normalizes a vector and prints it to the console (done in class)
     {
         return normalized.normalized;// returns the noramlized vector
     }
 
-    //public void Teleport(Vector2 jump)
-    //{
-    //    Vector2 direction = enemyTransform.position - playerPost.position;
+    public void WarpPlayer(Transform target, float ratio)// warps the player towards a target position based on a ratio
+    {
+        Vector3 startingPoint = playerPost.position;// stores the starting position of the player
+        Vector3 finalPoint = target.position;// stores the enemy position
 
-    //    //playerPost.position = playerPost.position + (Vector3)jump;
-    //}
-      
+        playerPost.position = Vector3.Lerp(startingPoint, finalPoint, ratio); // moves the player towards the enemy position based on the ratio
+    }
+
+
 }
